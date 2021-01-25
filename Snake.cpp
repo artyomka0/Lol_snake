202d11906f60d391dad6bcea7653be4acbe16d30
Artyomka <mikhailov.artyom.a@gmail.com>
2021-01-25 23:58:01 +0300

    create snake spawn
    some no correct, but work ))

--- Snake.cpp
+++ Snake.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
-
 using namespace std;
+
+#define xLenght 10
+#define yLenght 15
+#define seed 65
+
+class Snake
+{
+public:
+  //Snake();
+  //~Snake();
+
+  int nowLenght = 0;
+  int neilX;
+  int neilY;
+
+  void createNeil(int x, int y){
+    srand(seed);
+    this->neilX = rand() % x;
+    if (this->neilX == 0 || this->neilX > x-1-this->lenght)
+    {
+      this->neilX = x/2;
+    }
+    this->neilY = rand() % y;
+    if (this->neilY == 0 || this->neilY > y-1-this->lenght)
+    {
+      this->neilY = y/2;
+    }
+  };
+
+  int getLenght(){
+    return this->lenght;
+  }
+
+private:
+  int lenght = 4;
+};
 
 int main(int argc, char const *argv[]) {
 
@@ -10,22 +45,39 @@ int main(int argc, char const *argv[]) {
 
   cout << "Press " << endl;
   getchar();
-
   system("cls");
-  int xLenght = 50;
-  int yLenght = 100;
 
   char matrix[xLenght][yLenght];
-  int snakeLong = 4;
-
+  Snake snake;
+  snake.createNeil(xLenght,yLenght);
+  bool *SNAKE = new bool;
+  *SNAKE = false;
   for (int i = 0; i < xLenght; ++i) {
     for (int j = 0; j < yLenght; ++j) {
       if ((i == 0 || j == 0 || i == xLenght - 1 || j == yLenght - 1)) {
         matrix[i][j] = '#';
       }
+      else {
+        if ((i == snake.neilX && j == snake.neilY) || *SNAKE)
+        {
+           matrix[i][j] = '@';
+           *SNAKE = true;
+           snake.nowLenght++;
+           if (snake.nowLenght > snake.getLenght())
+           {
+             *SNAKE = false;
+           }
+        }
+        else {
+          matrix[i][j] = ' ';
+        }
+      }
+      cout << matrix[i][j];
     }
+    cout << endl;
   }
 
+  cout << snake.neilX << "   " << snake.neilY;
   bool gameEnd = false;
 
   while (!gameEnd) {
